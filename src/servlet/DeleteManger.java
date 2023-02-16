0b6a39e0e47edef12fa629e904738ce83c8a5511
yx4616 <yx4616@outlook.com>
2023-02-16 20:51:43 +0800

    更新

--- src/servlet/DeleteManger.java
+++ src/servlet/DeleteManger.java
@@ -13,8 +13,6 @@ public class DeleteManger extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
-        System.out.println(12);
-        System.out.println(id);
         String [] temp = id.split(",");
         for (String t:temp){
             new ManagerService().deleteManager(t);
